6724c5592a5c950f1a110ee60e18ebfb192f0d7c
choiby777 <choiby777@gmail.com>
2018-04-27 17:02:06 +0900

    코드 정렬

--- App.js
+++ App.js
@@ -1,96 +1,106 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, ActivityIndicator,StatusBar } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  StatusBar
+} from "react-native";
 import Weather from "./Weather";
 import Weather2 from "./WeatherFunction";
 
-const TEST_URL = "http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1";
+const TEST_URL =
+  "http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1";
 
 export default class App extends React.Component {
   state = {
-    isLoaded : false,
-    error : null,
-    temperature:null,
-    name : null,
-    isWeatherLoaded : false,
-  }
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null,
+    isWeatherLoaded: false
+  };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      position =>{
+      position => {
         this.setState({
           isLoaded: true
-        });      
+        });
       },
       error => {
         //console.log(error);
         this.setState({
           error: error
-        })              
-      }        
+        });
+      }
     );
-    
-    this._getWeather(1,0);
+
+    this._getWeather(1, 0);
   }
- 
-  _getWeather = (value1 , value2) => {
+
+  _getWeather = (value1, value2) => {
     fetch(TEST_URL)
-    .then(response => response.json())
-    .then(json => {
-      //console.log(json)
-      this.setState({
-        isWeatherLoaded : true,
-        temperature : json.list[0].main.temp,
-        name:json.city.name,
+      .then(response => response.json())
+      .then(json => {
+        //console.log(json)
+        this.setState({
+          isWeatherLoaded: true,
+          temperature: json.list[0].main.temp,
+          name: json.city.name
+        });
       });
-    });
   };
 
   render() {
-    const { isLoaded , error, temperature , name, isWeatherLoaded }  = this.state;
-    
+    const { isLoaded, error, temperature, name, isWeatherLoaded } = this.state;
+
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {isWeatherLoaded ? <Weather2 weatherName = 'Clear' temp={Math.floor(temperature - 273.15)}/> : (
-          <View style={styles.loading}>            
+        {isWeatherLoaded ? (
+          <Weather2
+            weatherName="Clear"
+            temp={Math.floor(temperature - 273.15)}
+          />
+        ) : (
+          <View style={styles.loading}>
             <Text style={styles.loadingText}> 로딩중!!!! </Text>
-            {error ? <Text style={styles.errorText}> {error} </Text> : null}            
+            {error ? <Text style={styles.errorText}> {error} </Text> : null}
             <Text style={styles.loadingText}> temp : {temperature} </Text>
             <Text style={styles.loadingText}> name : {name} </Text>
-            {isWeatherLoaded ? 
-              <Text style={styles.errorText}> 로드됨 </Text> : 
-              <Text style={styles.errorText}> 로드되지 않음 </Text>              
-            }                        
+            {isWeatherLoaded ? (
+              <Text style={styles.errorText}> 로드됨 </Text>
+            ) : (
+              <Text style={styles.errorText}> 로드되지 않음 </Text>
+            )}
           </View>
-        )}                
-      </View>     
+        )}
+      </View>
     );
-  };
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00ffff',
-    justifyContent: 'center',
-    flexDirection : 'column'
+    backgroundColor: "#00ffff",
+    justifyContent: "center",
+    flexDirection: "column"
   },
 
-  loading:{
-    flex : 1,
-    backgroundColor : '#fff19f',
+  loading: {
+    flex: 1,
+    backgroundColor: "#fff19f",
     paddingLeft: 25,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end"
   },
-  errorText:{
-    fontSize:25,      
-    fontWeight : 'bold',
-    color: 'red',
+  errorText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "red"
   },
-  loadingText:{
-    fontSize:38,      
-  },
-
-
-
+  loadingText: {
+    fontSize: 38
+  }
 });
--- WeatherFunction.js
+++ WeatherFunction.js
@@ -1,109 +1,116 @@
-import React, {Component} from 'react';
-import { StyleSheet , Text , View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
 import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 
 const weatherCases = {
-    Rain: {
-        colors : ["#8cc6ff" , "#0080ff"],
-        title: "Main Title Rain",
-        subTitle: "SubTile Rain",
-        icon: 'ios-sunny'
-    },
+  Rain: {
+    colors: ["#8cc6ff", "#0080ff"],
+    title: "Main Title Rain",
+    subTitle: "SubTile Rain",
+    icon: "ios-sunny"
+  },
 
-    Clear: {
-        colors : ["#8cc6ff" , "#fb46ae"],
-        title: "Main Title Clear",
-        subTitle: "SubTile Clear",
-        icon: 'ios-sunny'
-    },
+  Clear: {
+    colors: ["#8cc6ff", "#fb46ae"],
+    title: "Main Title Clear",
+    subTitle: "SubTile Clear",
+    icon: "ios-sunny"
+  },
 
-    Thunderstom: {
-        colors : ["#8cc6ff" , "#64d850"],
-        title: "Main Title Thunderstom",
-        subTitle: "SubTile Thunderstom",
-        icon: 'ios-thunderstom'
-    },
+  Thunderstom: {
+    colors: ["#8cc6ff", "#64d850"],
+    title: "Main Title Thunderstom",
+    subTitle: "SubTile Thunderstom",
+    icon: "ios-thunderstom"
+  },
 
-    Clouds: {
-        colors : ["#8cc6ff" , "#d9a211"],
-        title: "Main Title Clouds",
-        subTitle: "SubTile Clouds",
-        icon: 'ios-cloudy'
-    },
+  Clouds: {
+    colors: ["#8cc6ff", "#d9a211"],
+    title: "Main Title Clouds",
+    subTitle: "SubTile Clouds",
+    icon: "ios-cloudy"
+  },
 
-    Snow: {
-        colors : ["#f6f6f6" , "#bfbfbf"],
-        title: "Main Title Snow",
-        subTitle: "SubTile Snow",
-        icon: 'ios-snow'
-    },
+  Snow: {
+    colors: ["#f6f6f6", "#bfbfbf"],
+    title: "Main Title Snow",
+    subTitle: "SubTile Snow",
+    icon: "ios-snow"
+  }
+};
 
-}
+function Weather2({ weatherName, temp }) {
+  return (
+    <LinearGradient
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}
+    >
+      <View style={styles.top}>
+        <Ionicons
+          color="white"
+          size={144}
+          name={weatherCases[weatherName].icon}
+        />
+        <Text style={styles.TempText}> {temp}℃ </Text>
+      </View>
 
-function Weather2({ weatherName , temp }){
-    return(
-        <LinearGradient 
-            colors={weatherCases[weatherName].colors} 
-            style={ styles.container}
-        >                 
-            <View style={styles.top}>
-                <Ionicons color="white" size={144} name={weatherCases[weatherName].icon} />
-                <Text style={styles.TempText}> {temp}℃ </Text>
-            </View>
-
-            <View style={styles.bottom}>
-                <Text style={styles.bottomText1}> {weatherCases[weatherName].title} </Text>
-                <Text style={styles.bottomText2}> {weatherCases[weatherName].subTitle} </Text>
-            </View>
-        </LinearGradient>
-    );
+      <View style={styles.bottom}>
+        <Text style={styles.bottomText1}>
+          {" "}
+          {weatherCases[weatherName].title}{" "}
+        </Text>
+        <Text style={styles.bottomText2}>
+          {" "}
+          {weatherCases[weatherName].subTitle}{" "}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
 
 Weather2.PropTypes = {
-    temp : PropTypes.number.isRequired,
-    weatherName : PropTypes.string.isRequired
+  temp: PropTypes.number.isRequired,
+  weatherName: PropTypes.string.isRequired
 };
 
 export default Weather2;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },      
-    top:{
-        flex:3,
-        //backgroundColor: 'red',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection : 'column'    
-    },
-    TempText:{
-        fontSize:40,
-        //fontWeight : 'bold',
-        color: 'white',
-        textAlign: 'center',  
-        marginTop: 10,
-    },
-    bottom:{
-        flex:1,        
-        //backgroundColor: 'blue',
-        justifyContent: 'center',
-        flexDirection : 'column'    
-    },    
-    bottomText1:{
-        fontSize:40,
-        fontWeight : 'bold',
-        color: 'white',
-        textAlign: 'center'
-    },    
-    bottomText2:{
-        fontSize:20,
-        fontWeight : 'bold',
-        color: 'white',
-        textAlign: 'center'
-    },    
-
-  });
-  
+  container: {
+    flex: 1
+  },
+  top: {
+    flex: 3,
+    //backgroundColor: 'red',
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  },
+  TempText: {
+    fontSize: 40,
+    //fontWeight : 'bold',
+    color: "white",
+    textAlign: "center",
+    marginTop: 10
+  },
+  bottom: {
+    flex: 1,
+    //backgroundColor: 'blue',
+    justifyContent: "center",
+    flexDirection: "column"
+  },
+  bottomText1: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center"
+  },
+  bottomText2: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center"
+  }
+});
